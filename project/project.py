3377ad17f4ee7ef9fb54e2809350fbabfa123114
mohammed <mhmd.mahdi1@gmail.com>
2023-12-26 12:05:30 +0000

    testing AJAX

--- project/project.py
+++ project/project.py
@@ -1,23 +1,19 @@
 import logging
-from flask import Flask
+from flask import Flask, jsonify
 # import mysql.connector
 
 app = Flask(__name__)
 
-# Define a global database connection
-# db = None
-
-# Connect to the MySQL database
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="",
-#     database=""
-# )
+# Your endpoint for fetching all data
+@app.route('/get_all_data', methods=['GET'])
+def get_all_data():
+    # Your logic to fetch and return data
+    data = {"example": "data"}
+    return jsonify(data)
 
 @app.route('/')
 def index():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
