e6fd2d666edd3607d8401809b3162caf436a67c4
mohammed <mhmd.mahdi1@gmail.com>
2023-12-26 14:14:07 +0000

    Ajax testing

--- project/project.py
+++ project/project.py
@@ -1,14 +1,9 @@
 import logging
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
-# import mysql.connector
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)  # Enable CORS for all routes
 
-# Your endpoint for fetching all data
 @app.route('/get_all_data', methods=['GET'])
-@cross_origin(supports_credentials=True)  # Enable CORS for this specific route
 def get_all_data():
     # Your logic to fetch and return data
     data = {"example": "data"}
@@ -18,5 +13,5 @@ def get_all_data():
 def index():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
