da2617aedac31f0a42eeda179cc85711ca04a3c3
mohammed <mhmd.mahdi1@gmail.com>
2023-12-26 14:29:02 +0000

    testing AJAX

--- project/project.py
+++ project/project.py
@@ -1,17 +1,12 @@
-import logging
-from flask import Flask, jsonify
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-@app.route('/get_all_data', methods=['GET'])
-def get_all_data():
-    # Your logic to fetch and return data
-    data = {"example": "data"}
-    return jsonify(data)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
