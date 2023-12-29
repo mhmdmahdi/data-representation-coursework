dea3463176d4bac483f7d157bcf0e74e70ca9402
mohammed <mhmd.mahdi1@gmail.com>
2023-12-29 17:42:13 +0000

    Testing create

--- project/project.py
+++ project/project.py
@@ -2,22 +2,31 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-# Your endpoint for fetching all data
-@app.route('/get_all_data', methods=['GET'])
-def get_all_data():
-    # Your logic to fetch and return data
-    data = {"example1": "data"}
-    return jsonify(data)
-
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    # Your logic to fetch and return data
-    data = {"example2": "data"}
-    return jsonify(data)
 
 @app.route('/')
 def index():
     return render_template('personalbest.html')
+
+# Endpoint for fetching all data
+@app.route('/get_all_data', methods=['GET'])
+def get_all_data():
+    # Your logic to fetch and return data
+    data = {"example1": "data1"}
+    return jsonify(data)
+
+# Endpoint for creating
+@app.route('/post', methods=['POST'])
+def post():
+    # Your logic to fetch and return data
+    data = {"example2": "data2"}
+    return jsonify(data)
+
+# Endpoint for updating
+@app.route('/put', methods=['PUT'])
+def post():
+    # Your logic to fetch and return data
+    data = {"example3": "data3"}
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
